cd6ba1ccca2d3a7e77dc026cf4fd0ff3fcc78d9c
Leila Jorg Barbosa <leilajorg.barbosa@leilajorg.barbosa-C57CTF6Y06>
2022-10-14 12:36:43 +0800

    file systems implemented

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
-const fs = require('fs');
+const fs = require('fs')
 const process = require('process');
 
-const args = process.argv.slice(2).join(' ');
+const fileExist = process.argv[2]
+const secArg = process.argv[3]
+const choice = process.argv[4]
+const output = ''
 
-fs.readFile(args, 'utf8', (err, data) => {
-  if (err) {
-    throw err;
-  }
+if (fileExist === undefined) {
+  console.log('Requires an argument of path.')
+} else if (secArg === undefined) {
+  console.log('Second argument is required.')
+} else if (fileExist) {
+  fs.readFile(fileExist, 'utf8', (err, data) => {
+    if (err) {
+      throw err;
+    }
   const lineNumber = data.split('\n');
   for (let i = 0; i < lineNumber.length; i += 1) {
     console.log(`${i + 1} : ${lineNumber[i]}`);
   }
-});
+  })
+}
+if (fs.existsSync(secArg)){
+  if (choice !== undefined && choice !== '-n' && choice !== '-y') {
+    console.log('That is not a valid argument! (Expected -n, -y or undefined)')
+  } else if (choice === '-y') {
+      try {
+        fs.writeFileSync(secArg, output)
+      } catch (err) {
+          console.log('!!! Error: Problem in creating a file.')
+      }
+  } else if (choice === '-n' || choice == null) {
+    console.log('File not overwritten.')
+  } else {
+    try {
+      fs.writeFileSync(secArg, output)
+    } catch (err) {
+      console.log('!!! Error: Problem in Creating File')
+    }
+    }
+} 
