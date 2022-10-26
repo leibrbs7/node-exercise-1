537be7c6c3bcd0db9687e7d3492a8415045f683d
Leila Jorg Barbosa <leilajorg.barbosa@leilajorg.barbosa-C57CTF6Y06>
2022-10-26 09:13:37 +0800

    added linters

--- index.js
+++ index.js
@@ -7,14 +7,12 @@ const choice = process.argv[4]
 const output = ''
 
 if (fileExist === undefined) {
-  process.stdin.on("data", data => {
-    data = data.toString()
-    const lineNumber = data.split('\n');
+  process.stdin.on('data', (data) => {
+    const lineNumber = data.toString().split('\n');
     for (let i = 0; i < lineNumber.length; i += 1) {
       console.log(`${i + 1} : ${lineNumber[i]}`);
     }
   })
-
 } else if (secArg === undefined) {
   console.log('Second argument is required.')
 } else if (fileExist) {
@@ -22,21 +20,21 @@ if (fileExist === undefined) {
     if (err) {
       throw err;
     }
-  const lineNumber = data.split('\n');
-  for (let i = 0; i < lineNumber.length; i += 1) {
-    console.log(`${i + 1} : ${lineNumber[i]}`);
-  }
+    const lineNumber = data.split('\n');
+    for (let i = 0; i < lineNumber.length; i += 1) {
+      console.log(`${i + 1} : ${lineNumber[i]}`);
+    }
   })
 }
-if (fs.existsSync(secArg)){
+if (fs.existsSync(secArg)) {
   if (choice !== undefined && choice !== '-n' && choice !== '-y') {
     console.log('That is not a valid argument! (Expected -n, -y or undefined)')
   } else if (choice === '-y') {
-      try {
-        fs.writeFileSync(secArg, output)
-      } catch (err) {
-          console.log('!!! Error: Problem in creating a file.')
-      }
+    try {
+      fs.writeFileSync(secArg, output)
+    } catch (err) {
+      console.log('!!! Error: Problem in creating a file.')
+    }
   } else if (choice === '-n' || choice == null) {
     console.log('File not overwritten.')
   } else {
@@ -45,5 +43,5 @@ if (fs.existsSync(secArg)){
     } catch (err) {
       console.log('!!! Error: Problem in Creating File')
     }
-    }
+  }
 }
